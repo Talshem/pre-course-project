12e17b0e8563d66de1229e76b7503a6ac8a12d30
Talshem <talsh8797@gmail.com>
2020-08-06 20:53:43 +0300

    last fixes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 
-
 const prioritySelector = document.getElementById('prioritySelector');
 const textInput = document.getElementById('textInput');
 const addButton = document.getElementById('addButton');
 const viewSection = document.getElementById('viewSection');
-const resetButton = document.getElementById('resetButton')
+const resetButton = document.getElementById('resetButton');
 
+  if ( localStorage.length == 0 ){
+localStorage.setItem('j', 1);
+}
+
+var j = localStorage.getItem('j');
 var spanCount = 0;
-var j = 1;
+var jval = 1;
 
 textInput.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
@@ -45,6 +49,9 @@ const dDiv = document.createElement('div');
 dDiv.classList.add('todoCreatedAt');
 const tDiv = document.createElement('div');
 tDiv.classList.add('todoText');
+const closeButton = document.createElement('span');
+closeButton.classList.add('close');
+
 
 primaryDiv.appendChild(checking);
 primaryDiv.appendChild(pDiv);
@@ -54,10 +61,14 @@ primaryDiv.appendChild(dDiv);
 dDiv.textContent = date
 primaryDiv.appendChild(tDiv);
 tDiv.textContent = task;
+primaryDiv.appendChild(closeButton);
+closeButton.innerHTML = "&times;";
+
+closeButton.addEventListener('click', deleteItem, false);
 
 
 var divOpacity = 0;
-viewSection.appendChild(primaryDiv)
+viewSection.appendChild(primaryDiv);
 primaryDiv.style.opacity = '0';
 primaryDiv.firstChild.style.opacity = '1';
 
@@ -68,7 +79,15 @@ primaryDiv.style.opacity = divOpacity;
       divOpacity += 0.1; 
 }}
 
-localStorage.setItem(dDiv.textContent, tDiv.textContent);
+
+localStorage.setItem('pmemory' + j, pDiv.textContent);
+localStorage.setItem('dmemory' + j, dDiv.textContent);
+localStorage.setItem('tmemory' + j, tDiv.textContent);
+
+primaryDiv.setAttribute("id", "task" + j);
+
+j++;
+localStorage.setItem('j', j);
 
 addEventsDragAndDrop(primaryDiv);
 checking.addEventListener('click', taskFinished);
@@ -85,8 +104,8 @@ document.getElementById("counter").innerHTML = spanCount;
 function taskFinished(){
 
 var greenOpacity = 0;
-this.parentNode.lastElementChild.style.opacity = '0';
-var x = this.parentNode.lastElementChild;
+this.parentNode.children[3].style.opacity = '0';
+var x = this.parentNode.children[3];
 
 setInterval(greenBar, 35);
 function greenBar(){
@@ -96,33 +115,67 @@ x.style.opacity = greenOpacity;
 }}
 
 
-this.parentNode.lastElementChild.style.backgroundColor = '#4CAf99';
+this.parentNode.children[3].style.backgroundColor = '#4CAf99';
 this.parentNode.children[1].style.color = "rgb(76, 175, 153, 0)";
-this.parentNode.lastElementChild.style.textDecoration = "line-through";
+this.parentNode.children[3].style.textDecoration = "line-through";
+
 this.parentNode.children[1].innerHTML = '0';
+var findNumeral = this.parentNode.id.replace('task', '');
+localStorage.setItem('pmemory' + findNumeral, '0');
+
 this.parentNode.children[2].style.margin = '0px 17px 0px 17px';
-this.parentNode.lastElementChild.style.borderRadius = "5px" ;
+this.parentNode.children[3].style.borderRadius = "5px" ;
 spanCount --;
 document.getElementById("counter").innerHTML = spanCount;
 this.remove();
+
+const closeButton = document.createElement('span');
+closeButton.classList.add('close');
+primaryDiv.appendChild(closeButton);
+closeButton.addEventListener('click', deleteItem, false);
+
 }
+
+
 
 resetButton.onclick = function resetList(){
   var res = confirm("This action will delete all your tasks. Confirm to proceed.");
   if (res == true) {
+
 var node= document.getElementById("viewSection");
-while (node.firstChild) {
+jval = 1;
+
+     function opaqRemove() {
+if (node.firstChild)
+{
     node.removeChild(node.firstChild);
-    spanCount = 0;
-    document.getElementById("counter").innerHTML = spanCount;
+}
+else
+{
+clearInterval(resetInterval);
+}
+
+if ( spanCount > 0)
+{
+spanCount --;
+document.getElementById("counter").innerHTML = spanCount;
 
 }
-}
-}
+j = 1;
+localStorage.clear();
+}}
 
+var resetInterval = setInterval(opaqRemove, 50);
+
+  }
+  
 const sort = document.getElementById('sortButton');
 
-sort.onclick = function sortList() {
+
+sort.onclick = sortList;
+
+
+function sortList() {
  var switching, shouldSwitch;
  var list = document.getElementById("viewSection");
   switching = true;
@@ -206,7 +259,6 @@ function dragDrop(e) {
   }
   return false;
 }
- 
 function dragEnd(e) {
   var listItens = document.querySelectorAll('.draggable');
   [].forEach.call(listItens, function(item) {
@@ -218,9 +270,13 @@ function dragEnd(e) {
  
 
 function deleteItem(ev) {
- ev.preventDefault();
-  var kill = this;
+  var kill = this.parentNode;
   var pos = 0.75;
+
+var memoryDelete = kill.id.replace('task', '');
+localStorage.removeItem('tmemory' + memoryDelete);
+localStorage.removeItem('dmemory' + memoryDelete);
+localStorage.removeItem('pmemory' + memoryDelete);
 
  setInterval(frame, 35);
   function frame() {
@@ -229,15 +285,16 @@ function deleteItem(ev) {
        kill.remove();  
       }
 
-pos -= 0.05; 
+pos -= 0.05;
+kill.style.opacity = pos;
 kill.children[0].style.opacity = pos;
 kill.children[1].style.opacity = pos;
 kill.children[2].style.opacity = pos;
-kill.children[3].style.opacity = pos;
 }
 
-      if (kill.children[3]){
+      if (kill.children[4]){
       kill.children[0].remove();
+      kill.style.opacity = pos;
       kill.children[0].style.margin = '0px 0px 0px 37px';
       kill.children[1].style.margin = '0px 15px 0px 15px';
       kill.children[2].style.margin = '0px 15px 0px 15px';
@@ -254,7 +311,6 @@ function addEventsDragAndDrop(el) {
   el.addEventListener('dragleave', dragLeave, false);
   el.addEventListener('drop', dragDrop, false);
   el.addEventListener('dragend', dragEnd, false);
-  el.addEventListener('contextmenu', deleteItem, false);
 
 }
  
@@ -263,3 +319,72 @@ var listItens = document.querySelectorAll('.draggable');
   addEventsDragAndDrop(item);
 });
 
+  var jval = 1;
+
+onpageshow = function loadingHistory(){
+  while (jval < j ){
+
+
+const primaryDiv = document.createElement('div');
+const pDiv = document.createElement('div');
+pDiv.classList.add('todoPriority');
+const dDiv = document.createElement('div');
+dDiv.classList.add('todoCreatedAt');
+const tDiv = document.createElement('div');
+tDiv.classList.add('todoText');
+const closeButton = document.createElement('span');
+closeButton.classList.add('close');
+const checking = document.createElement("INPUT");
+checking.setAttribute("type", "checkbox");
+checking.classList.add('checking');
+
+
+primaryDiv.appendChild(pDiv);
+pDiv.textContent = localStorage.getItem('pmemory' + jval);
+primaryDiv.appendChild(dDiv);
+dDiv.textContent = localStorage.getItem('dmemory' + jval);
+primaryDiv.appendChild(tDiv);
+tDiv.textContent = localStorage.getItem('tmemory' + jval);
+primaryDiv.appendChild(closeButton);
+closeButton.innerHTML = "&times;";
+closeButton.addEventListener('click', deleteItem, false);
+
+
+primaryDiv.classList.add('todoContainer', 'draggable');
+var attr = document.createAttribute('draggable');
+primaryDiv.setAttributeNode(attr);
+attr.value = true;
+
+if (pDiv.textContent != '')
+{
+viewSection.appendChild(primaryDiv);
+primaryDiv.setAttribute("id", "task" + jval);
+primaryDiv.children[2].style.borderRadius = "5px";
+}
+
+if (Number(pDiv.innerHTML) == 0)
+{
+primaryDiv.children[2].style.backgroundColor = '#4CAf99';
+primaryDiv.children[0].style.color = "rgb(76, 175, 153, 0)";
+primaryDiv.children[2].style.textDecoration = "line-through";
+primaryDiv.children[1].style.margin = '0px 17px 0px 17px';
+primaryDiv.children[2].style.opacity = '0.75';
+}
+
+if (Number(pDiv.innerHTML) > 0 ){
+primaryDiv.prepend(checking);
+checking.addEventListener('click', taskFinished);
+  spanCount ++;
+  document.getElementById("counter").innerHTML = spanCount;
+}
+
+
+
+addEventsDragAndDrop(primaryDiv);
+jval++;
+  
+}
+
+sortList();
+
+}
